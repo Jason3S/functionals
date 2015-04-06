be83b48a1e5e6d39db314fd419a1598eb47e2f90
Jason Dent <jason@streetsidesoftware.nl>
2015-04-06 14:56:28 +0200

    Move the Autoloader to src.

---
+++ src/autoload.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ . '/fnGen.php');
+require_once(__DIR__ . '/functionals.php');
+require_once(__DIR__ . '/Sequence.php');
--- test/autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once('../src/fnGen.php');
-require_once('../src/functionals.php');
-require_once('../src/Sequence.php');
