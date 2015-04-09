dd7d11265fdb5f64c3a61c47e95d6a3793ed5473
Jason Dent <jason@streetsidesoftware.nl>
2015-04-09 10:06:51 +0200

    Add grammar tree and token parser.

--- src/SelectorPathParser.php
+++ src/SelectorPathParser.php
@@ -5,11 +5,16 @@ namespace functionals\Selector;
 use SebastianBergmann\GlobalState\Exception;
 
 class SelectorPathParser {
+    const TOKEN_START = 'start';
 	const TOKEN_WORD = 'word';
-	const TOKEN_FIELD_SEPERATOR = '.';
+	const TOKEN_FIELD_SEPARATOR = ',';
 	const TOKEN_CONDITION_BEGIN = '[';
 	const TOKEN_CONDITION_END = ']';
 	const TOKEN_OPERATOR_COMPARISON = '=';
+	const TOKEN_CONDITION_SEPARATOR = ',';
+    const TOKEN_GROUP_BEGIN = '(';
+    const TOKEN_GROUP_END = ')';
+    const TOKEN_END = 'end';
 	const TOKEN = 'token';
 	const VALUE = 'value';
 
@@ -19,11 +24,50 @@ class SelectorPathParser {
 		'~(\w+|\\\[\[\\\]*.\-=])+~A' => ['token' => self::TOKEN_WORD,                   'match' => 0],
 		'~\'([^\']+)\'~A'            => ['token' => self::TOKEN_WORD,                   'match' => 1],
 		'~"([^"]+)"~A'               => ['token' => self::TOKEN_WORD,                   'match' => 1],
-		'~\.~A'                      => ['token' => self::TOKEN_FIELD_SEPERATOR,        'match' => 0],
-		'~\[~A'                      => ['token' => self::TOKEN_CONDITION_BEGIN,        'match' => 0],
-		'~\]~A'                      => ['token' => self::TOKEN_CONDITION_END,          'match' => 0],
-		'~\s*=|<=|>=|<|>\s*~A'       => ['token' => self::TOKEN_OPERATOR_COMPARISON,    'match' => 0],
+		'~\.~A'                      => ['token' => self::TOKEN_FIELD_SEPARATOR,        'match' => 0],
+		'~\s*(\[)\s*~A'              => ['token' => self::TOKEN_CONDITION_BEGIN,        'match' => 1],
+		'~\s*(\])\s*~A'              => ['token' => self::TOKEN_CONDITION_END,          'match' => 1],
+        '~\s*(\()\s*~A'              => ['token' => self::TOKEN_GROUP_BEGIN,            'match' => 1],
+        '~\s*(\))\s*~A'              => ['token' => self::TOKEN_GROUP_END,              'match' => 1],
+		'~\s*(,|\|\|?|&&?)\s*~A'     => ['token' => self::TOKEN_CONDITION_SEPARATOR,    'match' => 1],
+		'~\s*(=|<=|>=|<|>)\s*~A'     => ['token' => self::TOKEN_OPERATOR_COMPARISON,    'match' => 1],
 	];
+
+    static $grammar = [
+        'start' => [
+            'selectors',
+        ],
+        'selectors' => [
+            'selector',
+            'selector field-separator selectors'
+        ],
+        'selector' => [
+            'word',
+            'word condition',
+            'condition',
+            'group-begin selector group-end',
+            'group-begin selector selector-operator selector group-end',
+        ],
+        'condition' => [
+            'condition-begin conditions condition-end',
+        ],
+        'conditions' => [
+            'word operator word',
+            'word operator conditions',
+            'group-begin conditions group-end',
+        ],
+
+        // leaf nodes
+        'word'                  => self::TOKEN_WORD,
+        'group-begin'           => self::TOKEN_GROUP_BEGIN,
+        'group-end'             => self::TOKEN_GROUP_END,
+        'operator'              => self::TOKEN_OPERATOR_COMPARISON,
+        'condition-operator'    => self::TOKEN_CONDITION_SEPARATOR,
+        'selector-operator'     => self::TOKEN_CONDITION_SEPARATOR,
+        'condition-begin'       => self::TOKEN_CONDITION_BEGIN,
+        'condition-end'         => self::TOKEN_CONDITION_END,
+        'field-separator'       => self::TOKEN_FIELD_SEPARATOR,
+    ];
 
 
 	public static function tokenize($path) {
@@ -45,6 +89,68 @@ class SelectorPathParser {
 
 		return $tokens;
 	}
+
+    public static function parseTokens($tokens) {
+        $grammar = self::$grammar;
+
+        $activeStatements = [
+            ['statements' => ['start'], 'offset' => 0, 'processed' => []]
+        ];
+
+
+        while (! empty($activeStatements)) {
+            $current = array_pop($activeStatements);
+
+            if ($current['offset'] == count($tokens)) {
+                if (empty($current['statements'])) {
+                    return $current['processed'];
+                } else {
+                    // We ran out of tokens to process, but more statements are expected, so this isn't the right branch.
+                    // Drop it.
+                    continue;
+                }
+            }
+
+            if (empty($current['statements'])) {
+                // We hit a leaf in the grammar tree, but there are still tokens.
+                continue;
+            }
+
+            $statement = array_shift($current['statements']);
+            $remainingStatements = $current['statements'];
+            $g = $grammar[$statement];
+            if (is_array($g)) {
+                $processed = $current['processed'];
+                $processed[] = [
+                    'statement' => $statement
+                ];
+                // Add them in reverse order since we pop them and want the simplest to be processed first.
+                foreach (array_reverse($g) as $statements) {
+                    $statements = explode(' ', $statements);
+                    $activeStatements[] = [
+                        'statements' => array_merge($statements, $remainingStatements),
+                        'offset' => $current['offset'],
+                        'processed' => $processed,
+                    ];
+                }
+            } else {
+                $offset = $current['offset'];
+                if ($g == $tokens[$offset][self::TOKEN]) {
+                    // We found a matching token.
+                    $current['processed'][] = [
+                        'statement' => $statement,
+                        'token' => $tokens[$offset]
+                    ];
+                    $current['offset'] += 1;
+                    $activeStatements[] = $current;
+                } else {
+                    // drop it, the token doesn't match.
+                }
+            }
+        }
+
+        return null;
+    }
 
 	public static function parsePath($path) {
 	}
--- test/SelectorPathParserExceptionTest.php
+++ test/SelectorPathParserExceptionTest.php
@@ -24,9 +24,18 @@ class SelectorPathParserExceptionTest extends \PHPUnit_Framework_TestCase {
 			],
 			'product.name' => [
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'product'],
-				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_FIELD_SEPERATOR,    SelectorPathParser::VALUE => '.'],
+				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_FIELD_SEPARATOR,    SelectorPathParser::VALUE => '.'],
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'name'],
 			],
+            '(product|item).name' => [
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_GROUP_BEGIN,        SelectorPathParser::VALUE => '('],
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'product'],
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_CONDITION_SEPARATOR,SelectorPathParser::VALUE => '|'],
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'item'],
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_GROUP_END,          SelectorPathParser::VALUE => ')'],
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_FIELD_SEPARATOR,    SelectorPathParser::VALUE => '.'],
+                [SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'name'],
+            ],
 			'products[type=55].name' => [
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'products'],
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_CONDITION_BEGIN,    SelectorPathParser::VALUE => '['],
@@ -34,13 +43,42 @@ class SelectorPathParserExceptionTest extends \PHPUnit_Framework_TestCase {
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_OPERATOR_COMPARISON,SelectorPathParser::VALUE => '='],
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => '55'],
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_CONDITION_END,      SelectorPathParser::VALUE => ']'],
-				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_FIELD_SEPERATOR,    SelectorPathParser::VALUE => '.'],
+				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_FIELD_SEPARATOR,    SelectorPathParser::VALUE => '.'],
 				[SelectorPathParser::TOKEN => SelectorPathParser::TOKEN_WORD,               SelectorPathParser::VALUE => 'name'],
 			],
 		];
 
 		foreach ($tests as $testPath => $expectedResult) {
-			$this->assertEquals($expectedResult, SelectorPathParser::tokenize($testPath));
+			$this->assertEquals($expectedResult, SelectorPathParser::tokenize($testPath), $testPath);
 		}
+
+		// test out spacing and quote issues
+		$this->assertEquals(SelectorPathParser::tokenize('products[type=55].name'), SelectorPathParser::tokenize('products[type = 55].name'));
+		$this->assertEquals(SelectorPathParser::tokenize('products[type="55"].name'), SelectorPathParser::tokenize('products[type = 55].name'));
+		$this->assertEquals(SelectorPathParser::tokenize('products[type="55"].name'), SelectorPathParser::tokenize('products[type = \'55\'].name'));
+		$this->assertEquals(
+			SelectorPathParser::tokenize('products[type=55&&price>"20.50"].name'),
+			SelectorPathParser::tokenize('products [type = 55 && price > "20.50"].name'));
 	}
+
+    public function testParseTokens() {
+        $tests = [
+            'product' => true,
+            '\'product\'' => true,
+            '"product"' => true,
+            'product.name' => true,
+            '(product|item).name' => true,
+            'products[type=55].name' => true,
+        ];
+
+        foreach ($tests as $testPath => $isSuccessExpected) {
+            $tokens = SelectorPathParser::tokenize($testPath);
+            $parsedTokens = SelectorPathParser::parseTokens($tokens);
+            if ($isSuccessExpected) {
+                $this->assertNotNull($parsedTokens, $testPath);
+            } else {
+                $this->assertNull($parsedTokens, $testPath);
+            }
+        }
+    }
 }
